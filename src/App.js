335ed78c020005d9bdd4ee421e965881a65be2c5
larrygf02 <raulgf_02@hotmail.com>
2019-11-02 21:12:56 -0500

    Creando formulario

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React, { useState} from 'react';
-
+import React, { useState, useEffect, Fragment } from 'react';
+import Formulario from './components/Formulario'
 function App() {
+  const [artista, agregarArtista] = useState('')
+  const [letra, agregarLetra ] = useState([])
+  const [info, agregarInfo ] = useState({})
+
+  // Consultar la API de la letra de canciones
+  const consultarAPILetra = busqueda => {
+    console.log(busqueda); 
+  }
+
   return (
-    <h1>Hola</h1>
+    <Fragment>
+      <Formulario consultarAPILetra={consultarAPILetra}></Formulario>
+    </Fragment>
   );
 }
 
